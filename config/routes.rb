6d956a6408dd3b17abd6728b9c37ed8e87322ce6
jordanfox1 <jordanfox9415@gmail.com>
2022-07-31 19:56:51 +1000

    created home/profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   devise_for :accounts
-  get "u/:username" => "profile#index", as: :profile
+  get "u/:username" => "home#profile", as: :profile
   
   resources :communities do
     resources :posts
