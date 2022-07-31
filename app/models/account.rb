2ada2592627caa54ee7b70a79f11aa1360dec69f
jordanfox1 <jordanfox9415@gmail.com>
2022-07-31 19:35:24 +1000

    created username

--- app/models/account.rb
+++ app/models/account.rb
@@ -5,6 +5,7 @@ class Account < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :communities
+  validates_presence_of :first_name, :last_name, :username
 
   def full_name
      "#{first_name} #{last_name}"
