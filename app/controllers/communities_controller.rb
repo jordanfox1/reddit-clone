02857837337fdb83f5f89d07d3b1bc721711c1cc
jordanfox1 <jordanfox9415@gmail.com>
2022-09-11 15:09:56 +1000

    debugging subscriptions

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -8,6 +8,9 @@ class CommunitiesController < ApplicationController
 
     def show
         @posts = @community.posts
+        @subscriber_count = @community.subscribers.count
+        # @is_subscribed = account_signed_in? ? Subscription.where(community_id: @community.id, account_id: current_account.id)
+        @subscription = Subscription.new
     end
 
     def new
